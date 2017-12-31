515511122bfb06e4df504a8bd7eafb9d98481835
Mithul Bhansali <mithul.bhansali80@gmail.com>
2017-12-31 12:17:11 +0530

    20C - King Movement Part I

--- src/org/mitz/chess/model/King.java
+++ src/org/mitz/chess/model/King.java
@@ -21,21 +21,18 @@ public class King extends Piece{
 	public boolean validateMove(Tile to) {
 		String message = "Piece '" + PIECE_DESC + "' movement invalid from " + this.getTile().getPosition() + " to " + to.getPosition();
 		boolean valid = false;
-//		Tile selfTile = this.getTile();
-//		int rankDiff = Math.abs(selfTile.getRank() - to.getRank());
-//		int fileIndexDiff = Math.abs(selfTile.getFileIndex() - to.getFileIndex());
-//		if((rankDiff == fileIndexDiff) || 
-//				((rankDiff > 0 && selfTile.getFileIndex() == to.getFileIndex()) ||
-//						(0 < fileIndexDiff && selfTile.getRank() == to.getRank())) && 
-//				(to.isEmpty() || (!to.isEmpty() && getColor() != to.getPiece().getColor()))) {
-//			message = "1 - Piece '" + PIECE_DESC + "' movement valid from " + this.getTile().getPosition() + " to " + to.getPosition();
-//			valid = true;
-//		}
-//		if(valid)
-//			System.out.println("King.validateMove(message): " + message);
-//		else
-//			System.err.println("King.validateMove(message): " + message);
-		return true;
+		Tile selfTile = this.getTile();
+		int rankDiff = Math.abs(selfTile.getRank() - to.getRank());
+		int fileIndexDiff = Math.abs(selfTile.getFileIndex() - to.getFileIndex());
+		if((rankDiff == 0 || rankDiff == 1) && (fileIndexDiff == 0 || fileIndexDiff == 1) && (to.isEmpty() || (to.getPiece().getColor() != getColor()))) {
+			message = "1 - Piece '" + PIECE_DESC + "' movement valid from " + this.getTile().getPosition() + " to " + to.getPosition();
+			valid = true;
+		}
+		if(valid)
+			System.out.println("King.validateMove(message): " + message);
+		else
+			System.err.println("King.validateMove(message): " + message);
+		return valid;
 	}
 
 }
