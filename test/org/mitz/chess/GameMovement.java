d1f992e38ac20cb5ae2b3f7c3a21e9b479907729
Mithul Bhansali <mithul.bhansali80@gmail.com>
2018-01-06 00:26:05 +0530

    36C - Refactoring in Test 'Game Movement' for simple valid movement and simple invalid movement

--- test/org/mitz/chess/GameMovement.java
+++ test/org/mitz/chess/GameMovement.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertNotSame;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mitz.chess.model.Board;
 import org.mitz.chess.model.Game;
@@ -11,27 +12,21 @@ import org.mitz.chess.model.Piece;
 import org.mitz.chess.model.Tile;
 
 public class GameMovement {
-
-	@Test
-	public void game1() {
-		Game game = new Game();
-		Board board = game.getBoard();
+	
+	private Game game;
+	private Board board;
+	
+	@Before
+	public void setUp() {
+		game = new Game();
+		board = game.getBoard();
+	}
+	
+	public void simpleValidMovement(char sourceFile, int sourceRank, char targetFile, int targetRank) {
 		Tile sourceTile = null;
 		Tile targetTile = null;
 		Piece sourcePiece = null;
 		Piece targetPiece = null;
-		
-		int sourceRank = -1;
-		char sourceFile = 'a';
-		
-		int targetRank = -1;
-		char targetFile = 'b';
-		
-		sourceRank = 2;
-		sourceFile = 'd';
-		
-		targetRank = 3;
-		targetFile = 'd';
 		
 		sourceTile = board.getTileAt(sourceRank, sourceFile);
 		sourcePiece = sourceTile.getPiece();
@@ -45,81 +40,34 @@ public class GameMovement {
 		
 		targetPiece = targetTile.getPiece();
 		assertSame("Source and Target Piece not same after valid movement", sourcePiece, targetPiece);
-
-		sourceRank = 7;
-		sourceFile = 'd';
-		
-		targetRank = 6;
-		targetFile = 'd';
-		
-		sourceTile = board.getTileAt(sourceRank, sourceFile);
-		sourcePiece = sourceTile.getPiece();
-		targetTile = board.getTileAt(targetRank, targetFile);
-
-		isMoveValid = game.move(sourceRank, sourceFile, targetRank, targetFile);
-		assertTrue("Movement valid", isMoveValid);
-		
-		assertTrue("Source Tile must be Empty", sourceTile.isEmpty());
-		assertTrue("Target Tile must not be Empty", !targetTile.isEmpty());
-		
-		targetPiece = targetTile.getPiece();
-		assertSame("Source and Target Piece not same after valid movement", sourcePiece, targetPiece);
-
-		sourceRank = 2;
-		sourceFile = 'e';
-		
-		targetRank = 4;
-		targetFile = 'e';
+	}
+	
+	public void invalidPieceMovement(char sourceFile, int sourceRank, char targetFile, int targetRank) {
+		Tile sourceTile = null;
+		Tile targetTile = null;
+		Piece sourcePiece = null;
+		Piece targetPiece = null;
 		
 		sourceTile = board.getTileAt(sourceRank, sourceFile);
 		sourcePiece = sourceTile.getPiece();
 		targetTile = board.getTileAt(targetRank, targetFile);
 
-		isMoveValid = game.move(sourceRank, sourceFile, targetRank, targetFile);
-		assertTrue("Movement valid", isMoveValid);
-		
-		assertTrue("Source Tile must be Empty", sourceTile.isEmpty());
-		assertTrue("Target Tile must not be Empty", !targetTile.isEmpty());
-		
-		targetPiece = targetTile.getPiece();
-		assertSame("Source and Target Piece not same after valid movement", sourcePiece, targetPiece);
-
-		sourceRank = 6;
-		sourceFile = 'd';
-		
-		targetRank = 5;
-		targetFile = 'd';
-		
-		sourceTile = board.getTileAt(sourceRank, sourceFile);
-		sourcePiece = sourceTile.getPiece();
-		targetTile = board.getTileAt(targetRank, targetFile);
-
-		isMoveValid = game.move(sourceRank, sourceFile, targetRank, targetFile);
-		assertTrue("Movement valid", isMoveValid);
-		
-		assertTrue("Source Tile must be Empty", sourceTile.isEmpty());
-		assertTrue("Target Tile must not be Empty", !targetTile.isEmpty());
-		
-		targetPiece = targetTile.getPiece();
-		assertSame("Source and Target Piece not same after valid movement", sourcePiece, targetPiece);
-		
-		sourceRank = 4;
-		sourceFile = 'e';
-		
-		targetRank = 6;
-		targetFile = 'e';
-		
-		sourceTile = board.getTileAt(sourceRank, sourceFile);
-		sourcePiece = sourceTile.getPiece();
-		targetTile = board.getTileAt(targetRank, targetFile);
-
-		isMoveValid = game.move(sourceRank, sourceFile, targetRank, targetFile);
-		assertTrue("Movement Invalid", !isMoveValid);
+		boolean isMoveValid = game.move(sourceRank, sourceFile, targetRank, targetFile);
+		assertTrue("Movement Invalid", !isMoveValid);	
 		
 		assertTrue("Source Tile must not be Empty", !sourceTile.isEmpty());
 		assertTrue("Target Tile must be Empty", targetTile.isEmpty());
 		
 		targetPiece = targetTile.getPiece();
-		assertNotSame("Source and Target Piece must not be same after invalid valid movement", sourcePiece, targetPiece);
+		assertNotSame("Source and Target Piece must not be same after invalid valid movement", sourcePiece, targetPiece);	
+	}
+	
+	@Test
+	public void game1() {
+		simpleValidMovement('d', 2, 'd', 3);
+		simpleValidMovement('d', 7, 'd', 6);
+		simpleValidMovement('e', 2, 'e', 4);
+		simpleValidMovement('d', 6, 'd', 5);
+		invalidPieceMovement('e', 4, 'e', 6);
 	}
 }
