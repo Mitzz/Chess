8b88258b782f3ad386b6c87c2c95a009a8939fb7
Mithul Bhansali <mithul.bhansali80@gmail.com>
2018-01-11 23:09:13 +0530

    44C - Castling Feature : Two Test Cases for Castling when white king is check

--- test/org/mitz/chess/CastlingMovement.java
+++ test/org/mitz/chess/CastlingMovement.java
@@ -28,7 +28,6 @@ public class CastlingMovement {
 		simpleValidMovement('c', 7, 'c', 5);
 		
 		castlingValidMovement('e', 1, 'g', 1);
-		board.render();
 	}
 	
 	@Test
@@ -59,7 +58,21 @@ public class CastlingMovement {
 		simpleValidMovement('f', 1, 'e', 1);
 		simpleValidMovement('d', 7, 'd', 5);
 		invalid('e', 1, 'g', 1);
-		
+	}
+	
+	@Test
+	public void whiteKingsideCastlingNonvalidMovementDueToKingCheck() {
+		simpleValidMovement('g', 1, 'f', 3);
+		simpleValidMovement('g', 8, 'f', 6);
+		simpleValidMovement('e', 2, 'e', 3);
+		simpleValidMovement('b', 7, 'b', 6);
+		simpleValidMovement('f', 1, 'e', 2);
+		simpleValidMovement('c', 7, 'c', 6);
+		simpleValidMovement('d', 2, 'd', 4);
+		simpleValidMovement('e', 7, 'e', 6);
+		simpleValidMovement('e', 3, 'e', 4);
+		simpleValidMovement('f', 8, 'b', 4);
+		invalid('e', 1, 'g', 1);
 	}
 	
 	@Test
@@ -73,6 +86,28 @@ public class CastlingMovement {
 		simpleValidMovement('b', 1, 'c', 3);
 		simpleValidMovement('g', 8, 'f', 6);
 		castlingValidMovement('e', 1, 'c', 1);
+		
+	}
+	
+	@Test
+	public void whiteQueensideCastlingNonvalidMovementDueToKingCheck() {
+		simpleValidMovement('g', 1, 'f', 3);
+		simpleValidMovement('g', 8, 'f', 6);
+		simpleValidMovement('e', 2, 'e', 3);
+		simpleValidMovement('b', 7, 'b', 6);
+		simpleValidMovement('f', 1, 'e', 2);
+		simpleValidMovement('c', 7, 'c', 6);
+		simpleValidMovement('d', 2, 'd', 4);
+		simpleValidMovement('e', 7, 'e', 6);
+		simpleValidMovement('e', 3, 'e', 4);
+		simpleValidMovement('f', 8, 'd', 6);
+		simpleValidMovement('b', 1, 'a', 3);
+		simpleValidMovement('d', 6, 'f', 8);
+		simpleValidMovement('d', 1, 'd', 3);
+		simpleValidMovement('f', 8, 'd', 6);
+		simpleValidMovement('c', 1, 'e', 3);
+		simpleValidMovement('d', 6, 'b', 4);
+		invalid('e', 1, 'c', 1);
 		game.render();
 	}
 	
