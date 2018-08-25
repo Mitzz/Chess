69520bbeaaff480c36a8a9c0d637f9c15c4fab1a
Unknown <mithul.bhansali80@gmail.com>
2018-08-25 20:22:36 +0530

    64C - View Creation
    Adding chessboard, buttons and labels to the outer panel. Main challenge of Component Layout w.r.t. to each other

---
+++ src/org/mitz/chess/view/ChessBoard.java
@@ -0,0 +1,9 @@
+package org.mitz.chess.view;
+
+import javax.swing.JPanel;
+
+public class ChessBoard extends JPanel{
+
+	public ChessBoard() {
+	}
+}
--- src/org/mitz/chess/view/ChessPanel.java
+++ src/org/mitz/chess/view/ChessPanel.java
@@ -1,9 +1,48 @@
 package org.mitz.chess.view;
 
+import java.awt.Color;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class ChessPanel extends JPanel{
+public class ChessPanel extends JPanel {
 
 	public ChessPanel() {
+		setLayout(null);
+		setBackground(new Color(0, 100, 0));
+		
+		ChessBoard board = new ChessBoard();
+		board.setBounds(25, 30, 252, 252);
+		add(board);
+		
+		JButton newGameBtn = new JButton("New Game");
+		newGameBtn.setBounds(300, 70, 110, 24);
+		add(newGameBtn);
+		
+		JButton resignBtn = new JButton("Resign");
+		resignBtn.setBounds(300, 130, 110, 24);
+		add(resignBtn);
+		
+		JLabel player2Label = new JLabel("Player 2");
+		player2Label.setBounds(130, 8, 210, 24);
+		player2Label.setForeground(Color.CYAN);
+		add(player2Label);
+		
+		JLabel player1Label = new JLabel("Player 1");
+		player1Label.setBounds(130, 280, 210, 24);
+		player1Label.setForeground(Color.CYAN);
+		add(player1Label);
+		
+		JLabel gameStatusLabel = new JLabel("Click 'New Game' to start new game");
+		gameStatusLabel.setBounds(30, 315, 210, 24);
+		gameStatusLabel.setForeground(Color.CYAN);
+		add(gameStatusLabel);
+		
+		JLabel movementStatusLabel = new JLabel("Movement Status");
+		movementStatusLabel.setBounds(30, 350, 210, 24);
+		movementStatusLabel.setForeground(Color.CYAN);
+		add(movementStatusLabel);
+		
 	}
 }
